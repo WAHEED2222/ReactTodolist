91478fff7226ff9122b3f4caccf67fbf3b738764
WAHEED2222 <hayomide283@gmail.com>
2023-08-30 10:15:41 -0700

    third comit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import TaskList from "./TaskList";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState('');
+
+  // Load tasks from local storage when the component mounts
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTodoList(savedTasks);
+  }, []);
+
+  // Save tasks to local storage whenever the todoList state changes
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(todoList));
+  }, [todoList]);
 
   const handleInputChange = (event) => {
     setNewTask(event.target.value);
@@ -26,8 +37,15 @@ function App() {
   return (
     <div className="App">
       <div className="addTask">
-        <input onChange={handleInputChange} value={newTask} className="input" placeholder="Add Your Todolist"></input>
-        <button onClick={addTask} className="btnAddtask">Add Task</button>
+        <input
+          onChange={handleInputChange}
+          value={newTask}
+          className="input"
+          placeholder="Add Your Todolist"
+        ></input>
+        <button onClick={addTask} className="btnAddtask">
+          Add Task
+        </button>
       </div>
       <TaskList todoList={todoList} onDelete={deleteTask} />
     </div>
